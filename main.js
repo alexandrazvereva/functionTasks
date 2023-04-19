86070feac5ced8dba18673681bf7a725b900f858
LenaYork <lenayork@tut.by>
2023-04-19 11:43:30 +0300

    feat: add tasks

--- main.js
+++ main.js
@@ -13,19 +13,11 @@ showWordsLength["Anna","Elizabeth", "William", "Yo"] // [4,8, 6, 2]
 
 function showAverage(array) {
     //ваш код 
-    let sum = 0;
-    console.log("array")
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
-    console.log(Math.round(sum / array.length))
-    return sum / array.length;
 }
 
-showAverage([1, 5, 10]); 
-console.log(showAverage[1, 5, 10]) // 5
-console.log(showAverage[6, 32, 100]) // 46
-console.log(showAverage[400, 7, 182]) // 745
+console.log(showAverage([1, 5, 10])) // 5
+console.log(showAverage([6, 32, 100])) // 46
+console.log(showAverage([400, 7, 1829])) // 745
 
 //3. Напишите функцию, которая принимает 2 параметра - 2 массива. 
 //Функция должна склеивать их в один массив и выводить в консоль.
